96207227ceaeff1ef470cf9baf21781efd5f0742
Loïc TRUCHOT <loic@truchot.be>
2019-01-29 19:58:01 +0100

    feat(master): finalize infinite quiz

--- src/Main.elm
+++ src/Main.elm
@@ -1,39 +1,31 @@
-module Main exposing (Model, Msg(..), QuizItem, init, main, update, view)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Array exposing (Array)
 import ArrayHelper
 import Browser
-import Html exposing (Html, button, div, p, text)
+import Html exposing (Html, button, div, h1, h2, img, text)
+import Html.Attributes exposing (class, disabled, src)
 import Html.Events exposing (onClick)
-import Quiz exposing (QuizQa, periodicTable)
-
-
-type alias QuizItem =
-    { qa : QuizQa
-    , answers : List String
-    }
-
-
-pickQuizQa : Int -> Array QuizQa -> Array QuizQa -> ( QuizItem, Array QuizQa )
-pickQuizQa randomIdx qas allQas =
-    let
-        ( qa, remainingQas ) =
-            ArrayHelper.sliceByIndex qas randomIdx
-    in
-    ( { qa = Maybe.withDefault ( "Question", "Answer" ) qa
-      , answers = []
-      }
-    , remainingQas
-    )
+import Quiz exposing (QuizItem, QuizQa, belgianBirdsQuiz, pickQuizQa)
+import Time
 
 
 main =
     Browser.element
         { init = init
         , update = update
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = subscriptions
         , view = view
         }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    if model.waitNextQuestion then
+        Time.every 2000 (always PickNextQuestion)
+
+    else
+        Sub.none
 
 
 
@@ -45,20 +37,24 @@ type alias Model =
     , remainingQuizQas : Array QuizQa
     , currentQuizItem : QuizItem
     , status : String
+    , waitNextQuestion : Bool
+    , chosenAnswer : String
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { quizQas = periodicTable
-      , remainingQuizQas = periodicTable
+    ( { quizQas = belgianBirdsQuiz
+      , remainingQuizQas = belgianBirdsQuiz
       , currentQuizItem =
-            { qa = ( "Question", "Answer" )
+            { qa = ( "", "" )
             , answers = []
             }
-      , status = "Try this quizz."
+      , status = "Trouve la bonne réponse..."
+      , waitNextQuestion = False
+      , chosenAnswer = ""
       }
-    , ArrayHelper.provideRandomElt DisplayNextQuestion periodicTable
+    , ArrayHelper.provideRandomElt DisplayNextQuestion belgianBirdsQuiz
     )
 
 
@@ -68,6 +64,7 @@ init _ =
 
 type Msg
     = DisplayNextQuestion Int
+    | PickNextQuestion
     | CheckAnswer String
     | SetOtherAnswers (Array QuizQa)
     | DisplayAnswers (Array QuizQa)
@@ -76,6 +73,32 @@ type Msg
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        CheckAnswer answer ->
+            let
+                status =
+                    if answer == Tuple.second model.currentQuizItem.qa then
+                        "Bien joué !"
+
+                    else
+                        "Perdu..."
+            in
+            ( { model
+                | chosenAnswer = answer
+                , status = status
+                , waitNextQuestion = True
+              }
+            , Cmd.none
+            )
+
+        PickNextQuestion ->
+            ( { model
+                | waitNextQuestion = False
+                , chosenAnswer = ""
+                , status = ""
+              }
+            , ArrayHelper.provideRandomElt DisplayNextQuestion model.remainingQuizQas
+            )
+
         DisplayNextQuestion randomIdx ->
             let
                 ( chosen, remaining ) =
@@ -90,40 +113,34 @@ update msg model =
                     else
                         model.quizQas
               }
-            , ArrayHelper.provideShuffled SetOtherAnswers (Array.filter (\val -> (Tuple.first val) /= (Tuple.first chosen.qa)) model.quizQas)
+            , ArrayHelper.provideShuffled SetOtherAnswers (Array.filter (\val -> Tuple.first val /= Tuple.first chosen.qa) model.quizQas)
             )
 
-        CheckAnswer answer ->
-            let
-                status =
-                    if answer == (Tuple.second model.currentQuizItem.qa) then
-                        "Bravo"
-
-                    else
-                        "Wrong"
-            in
-            ( { model | status = status }, ArrayHelper.provideRandomElt DisplayNextQuestion model.remainingQuizQas )
-
         SetOtherAnswers arr ->
-          let
-              item = model.currentQuizItem
-              others = Array.slice 0 3 arr
-          in
-        
-            (model
-            , ArrayHelper.provideShuffled DisplayAnswers (Array.push item.qa others))
+            let
+                item =
+                    model.currentQuizItem
+
+                others =
+                    Array.slice 0 3 arr
+            in
+            ( model
+            , ArrayHelper.provideShuffled DisplayAnswers (Array.push item.qa others)
+            )
 
         DisplayAnswers arr ->
-          let
-              item = model.currentQuizItem
-          in
-        
-            ({ model
+            let
+                item =
+                    model.currentQuizItem
+            in
+            ( { model
                 | currentQuizItem =
                     { item
                         | answers = Array.toList (Array.map Tuple.second arr)
                     }
-            }, Cmd.none)
+              }
+            , Cmd.none
+            )
 
 
 
@@ -132,12 +149,60 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ p [] [ text (Tuple.first model.currentQuizItem.qa) ]
-        , div []
+    div [ class "container" ]
+        [ div [ class "col" ]
+            [ h1 []
+                [ text "Oiseaux de Belgique" ]
+            , h2 []
+                [ text "Mode « infini »" ]
+            ]
+        , div [ class "row" ]
+            [ div [ class "col p-3" ]
+                [ img
+                    [ class "img-h-250"
+                    , let
+                        imgSrc =
+                            Tuple.first model.currentQuizItem.qa
+                      in
+                      src
+                        (if imgSrc /= "" then
+                            imgSrc
+
+                         else
+                            "assets/img/loading.gif"
+                        )
+                    ]
+                    []
+                ]
+            ]
+        , div [ class "row" ]
             (List.map
-                (\answer -> button [ onClick (CheckAnswer answer) ] [ text answer ])
+                (\answer ->
+                    div [ class "col-6 col-md-3 p-1 min-h-80" ]
+                        [ div [ class "h-100" ]
+                            [ button
+                                [ class
+                                    ("btn-ws-normal btn btn-lg btn-block h-100"
+                                        ++ (if not model.waitNextQuestion then
+                                                " btn-light "
+
+                                            else if answer == Tuple.second model.currentQuizItem.qa then
+                                                " btn-success"
+
+                                            else
+                                                " btn-danger"
+                                           )
+                                    )
+                                , disabled model.waitNextQuestion
+                                , onClick (CheckAnswer answer)
+                                ]
+                                [ text answer ]
+                            ]
+                        ]
+                )
                 model.currentQuizItem.answers
             )
-        , p [] [ text model.status ]
+        , div [ class "row" ]
+            [ div [ class "col text-center p-3" ] [ text model.status ]
+            ]
         ]
--- src/Quiz.elm
+++ src/Quiz.elm
@@ -1,14 +1,34 @@
-module Quiz exposing (QuizQa, periodicTable)
+module Quiz exposing (QuizItem, QuizQa, belgianBirdsQuiz, periodicTableQuiz, pickQuizQa)
 
 import Array exposing (Array)
+import ArrayHelper
 
 
 type alias QuizQa =
     ( String, String )
 
 
-periodicTable : Array QuizQa
-periodicTable =
+type alias QuizItem =
+    { qa : QuizQa
+    , answers : List String
+    }
+
+
+pickQuizQa : Int -> Array QuizQa -> Array QuizQa -> ( QuizItem, Array QuizQa )
+pickQuizQa randomIdx qas allQas =
+    let
+        ( qa, remainingQas ) =
+            ArrayHelper.sliceByIndex qas randomIdx
+    in
+    ( { qa = Maybe.withDefault ( "", "" ) qa
+      , answers = []
+      }
+    , remainingQas
+    )
+
+
+periodicTableQuiz : Array QuizQa
+periodicTableQuiz =
     Array.fromList
         [ ( "Hélium", "He" )
         , ( "Hydrogène", "H" )
@@ -19,3 +39,41 @@ periodicTable =
         , ( "Azote", "N" )
         , ( "Oxygène", "O" )
         ]
+
+
+belgianBirdsQuiz : Array QuizQa
+belgianBirdsQuiz =
+    Array.fromList (List.map (\bird -> ( "assets/img/" ++ bird.id ++ ".jpg", bird.vernacularName )) belgianBirds)
+
+
+type alias ImgQuizzItem =
+    { id : String
+    , vernacularName : String
+    , binominalName : String
+    , license : String
+    }
+
+
+belgianBirds : List ImgQuizzItem
+belgianBirds =
+    [ { id = "bb59"
+      , vernacularName = "Accenteur mouchet"
+      , binominalName = "Prunella modularis"
+      , license = "Par Karstenderivative work: Wenkbrauwalbatros (talk) — CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=7023601"
+      }
+    , { id = "bb60"
+      , vernacularName = "Alouette des champs"
+      , binominalName = "Alauda arvensis"
+      , license = "Par Daniel Pettersson — Picture taken by Daniel Pettersson. Uploaded to commons by oskila with his permission. File taken from http://www.fagelfoto.se, CC BY-SA 2.5 se, https://commons.wikimedia.org/w/index.php?curid=1722926"
+      }
+    , { id = "bb61"
+      , vernacularName = "Autour des palombes"
+      , binominalName = "Accipiter gentilis"
+      , license = "Par Elon Howard Eaton (1866-1935, author), Louis Agassiz Fuertes (artist, 1874-1927) — Birds of New York (New York State Museum. Memoir 12), Albany: University of the State of New York. Plates by Fuertes later reproduced in Birds of America (1917?) by Thomas Gilbert Pearson (1873-1943) et al., Domaine public, https://commons.wikimedia.org/w/index.php?curid=715780"
+      }
+    , { id = "bb62"
+      , vernacularName = "Avocette élégante"
+      , binominalName = "Recurvirostra avosetta"
+      , license = "Par Andreas Trepte — Travail personnel, CC BY-SA 2.5, https://commons.wikimedia.org/w/index.php?curid=10610115"
+      }
+    ]
